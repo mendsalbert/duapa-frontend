8d23328a3b264efeed647ab85c8cf2ea4e93acec
mendsalbert <mendsalbert@gmail.com>
2022-10-25 23:51:46 +0000

    frontend

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import store from "./Redux/store";
 import { Provider } from "react-redux";
 import axios from "axios";
-axios.defaults.baseURL = "https://duapa.herokuapp.com ";
+axios.defaults.baseURL = "https://duapa.herokuapp.com";
 ReactDOM.render(
   <Provider store={store}>
     <App />
